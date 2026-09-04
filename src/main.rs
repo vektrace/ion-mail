884715342d22902e21d51b3acb929c8ea0882a5c
Vektrace <154304377+vektrace@users.noreply.github.com>
2026-09-04 22:14:44 +0200

    chore: change username

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 /*
     ion-mail  Mail CLI in Rust supporting all mail functions & OAuth2 login
-    Copyright (C) 2026  Paul8711
+    Copyright (C) 2026  Vektrace
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as
